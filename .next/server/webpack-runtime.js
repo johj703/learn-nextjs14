d2f08e95b5517594bd8a67b871afd6cc258a6296
Jo <johj703@nate.com>
2024-03-26 21:18:15 +0900

    page.tsx를 (home) 디렉토리로 이동

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("915d5d616218d80d")
+/******/ 		__webpack_require__.h = () => ("7ff47d80b5becfe4")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import Navigation from "../components/navigation";
 import SalesLayout from "./about-us/company/jobs/sales/layout";
 import Sales from "./about-us/company/jobs/sales/page";
-import AboutUsLayout from "./about-us/layout";
 
 export const metadata = {
   title: "Next.js",
