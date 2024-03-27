f9b177d55e518f0a0b96afa770aa3aa71d2acdaf
Jo <johj703@nate.com>
2024-03-27 22:25:51 +0900

    metadata 추가

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,9 @@
-
+export const metadata = {
+  title: "Home"
+}
 
 const URL = "https://nomad-movies.nomadcoders.workers.dev/movies
 
 export default function Page() {
-  
   return <div>{JSON.stringify(movies)}</div>;
 }
