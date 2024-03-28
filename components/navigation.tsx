2a59758d500112c940c118bb08a1350f86904363
Jo <johj703@nate.com>
2024-03-28 20:35:47 +0900

    사용하지 않는 count state 삭제

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -5,7 +5,6 @@ import { useState } from "react";
 
 export default function Navigation() {
   const path = usePathname();
-  const [count, setCount] = useState(0);
   return (
     <nav>
       <li>
