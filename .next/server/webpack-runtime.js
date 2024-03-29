d5cc93a21551f6d956dcc28bff7187b5aad62801
Jo <johj703@nate.com>
2024-03-29 17:04:44 +0900

    에러를 나타내는 구문 삭제하고 데이터를 fetch하도록 주석처리한 부분 제거하기

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("c9e8800c31801e79")
+/******/ 		__webpack_require__.h = () => ("7c3ca6109525fb34")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
