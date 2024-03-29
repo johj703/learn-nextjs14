08c935928185ad61d521519ce529a6817e82f030
Jo <johj703@nate.com>
2024-03-29 16:30:02 +0900

    import Link from "next/link"; 삭제

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("6749ec53194d6f65")
+/******/ 		__webpack_require__.h = () => ("31270263a3349083")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import Movie from "../../components/movie";
 
 export const metadata = {
