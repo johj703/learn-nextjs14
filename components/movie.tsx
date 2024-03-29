6ce8b3f3ee7833dd7775d52f576b39edc79f519e
Jo <johj703@nate.com>
2024-03-29 16:12:11 +0900

    Movie 컴포넌트 작성

---
+++ components/movie.tsx
@@ -0,0 +1,14 @@
+interface IMovieProps {
+    title: string;
+    id: string;
+    poster_path: string
+}
+
+export default function Movie({title, id, poster_path}: IMovieProps) {
+    return (
+    <div>
+        <img src={poster_path} alt={title} />
+        <Link href={`/movies/${id}`}>{title}</Link>
+      </div>
+    );
+}
