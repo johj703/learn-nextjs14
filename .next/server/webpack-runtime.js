ad423663917112340f198d7ed7c471f9b46a4fe6
Jo <johj703@nate.com>
2024-03-29 18:21:05 +0900

    평점을 소수점 아래 한자리로 보이도록 작성

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("ddd1b2e8dce401d5")
+/******/ 		__webpack_require__.h = () => ("b26da564191bfaad")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
