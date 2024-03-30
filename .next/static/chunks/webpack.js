10665394abb774ec365842af906a5c30e705be89
Jo <johj703@nate.com>
2024-03-30 11:32:12 +0900

    movie-videos의 container에 css 추가

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1ccb1868bddcfd58"; }
+/******/ 		__webpack_require__.h = function() { return "f6996b07c7cca5b6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
